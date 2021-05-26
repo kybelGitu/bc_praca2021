e688504a3b23f9ff645e3d9d9cd0f4d32b0105f6
kybelGitu <albrechtjasovsky@gmail.com>
2021-05-26 22:21:03 +0200

    som funstions to display, moved constants to separete file

--- GDK101_project/src/lib/fun.py
+++ GDK101_project/src/lib/fun.py
@@ -1,5 +1,6 @@
 from machine import I2C, Pin
 import time
+import consts
 
 #FIND DEVICE BY ADDR
 def DETECT_DEVICE(_i2c, device_addr):
@@ -26,5 +27,31 @@ def DATA_TRANSFER(_i2c,  device_addr, command):
         data = _i2c.readfrom_mem(device_addr, command, 2)
     return data
 
-
 #def RESET_DEVICE(_i2c, device_addr):
+
+def DISPLAY_VERSION(_i2c, lcd, device_addr, location):
+    #test if location is not out od bounds
+    if(location < 0 or location > consts.MAX_LOCATION):
+        raise Exception('location {} is out of bounds: '.format(location))
+    data = _i2c.readfrom_mem(device_addr, consts.READ_FIRMWARE_VERSION, 2)
+    version = "version: "+str(data[0]) + '.' + str(data[1])
+    print(version)
+    #coordinates is calculated by int division and reminder
+    column = location % consts.DISPLAY_WIDTH
+    line = location // consts.DISPLAY_WIDTH
+    DISPLAY_TEXT(lcd, version, column, line)
+
+
+def DISPLAY_TEXT(lcd, text, x_pos, y_pos):
+    y_pos = y_pos - consts.LETTER_SIZE  if (y_pos + consts.LETTER_SIZE  > consts.DISPLAY_HEIGTH ) else y_pos 
+    textLengthPixels = len(text)*consts.LETTER_SIZE
+    x_pos = x_pos - textLengthPixels  if (x_pos + textLengthPixels  > consts.DISPLAY_WIDTH ) else x_pos 
+    #text is too wide
+    if(x_pos < 0):
+        x_pos = 0
+    lcd.text(text, x_pos, y_pos)
+    lcd.show()
+    time.sleep(2)
+
+
+
--- GDK101_project/src/main.py
+++ GDK101_project/src/main.py
@@ -2,15 +2,7 @@ from machine import I2C,Pin
 import ssd1306
 import fun
 import time
-
-LCD_ADDR = 0x3c
-GDK101_ADDR = 0X18
-RESET_SENZOR = 0XA0
-READ_STATUS = 0XB0
-READ_MEASURING_TIME = 0XB1
-READ_MEASURING_VALUE_TEN_MIN = 0XB2
-READ_MEASURING_VALUE_ONE_MIN = 0XB3
-READ_FIRMWARE_VERSION = 0XB4
+import consts
 
 rst = Pin(16, Pin.OUT)
 rst.value(1)
@@ -22,12 +14,14 @@ i2c = I2C(scl=scl, sda=sda)
 
 lcd = ssd1306.SSD1306_I2C(128, 64, i2c)
 
-result = fun.DATA_TRANSFER(i2c, GDK101_ADDR, READ_STATUS)
+result = fun.DATA_TRANSFER(i2c, consts.GDK101_ADDR, consts.READ_STATUS)
 # print("naslo sa " + str(result))
 print("naslo sa " + str(result))
 
 
 
-lcd.text("rslt "+ str(result), 0, 0)
-lcd.show()
+# lcd.text("rslt "+ str(result), 3, 116)
+# lcd.show()
+
+fun.DISPLAY_VERSION(i2c, lcd, consts.GDK101_ADDR, 500)
 # while True:
