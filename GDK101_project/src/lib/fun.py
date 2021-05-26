3e4860a1a8c53454b2efa53e815ffbdf60651770
kybelGitu <albrechtjasovsky@gmail.com>
2021-05-26 15:02:01 +0200

    DATA_TRANSFER function and added timer lib

--- GDK101_project/src/lib/fun.py
+++ GDK101_project/src/lib/fun.py
@@ -1,8 +1,30 @@
+from machine import I2C, Pin
+import time
+
 #FIND DEVICE BY ADDR
 def DETECT_DEVICE(_i2c, device_addr):
     _is_present = False
-    _i2c_peripheries = _i2c.scan()
+    _i2c_peripheries = _i2c.scan() 
+    time.sleep(1) # after scan need some time, otherwise OSError: [Errno 110] ETIMEDOUT
     for _i2c_periphery in _i2c_peripheries:    
       if (_i2c_periphery == device_addr):
         _is_present = True
     return _is_present
+
+#INPUT: I2C, DEVICE_ADDR, COMMAND
+#RETURN: DATA FROM I2C
+# EXCEPTION IS GENERATED IF NO DEVICE WAS FOU ND
+# DETECT DEVICE
+# SEND DATA TO DEVICE
+# READ DATA
+def DATA_TRANSFER(_i2c,  device_addr, command):
+    # data = _i2c.readfrom_mem(0X18, 0XB1, 2)
+    data = _i2c.readfrom_mem(device_addr, command, 2)
+    if not DETECT_DEVICE(_i2c, device_addr):
+        raise Exception('device with addr {} not found: '.format(device_addr))
+    else:
+        data = _i2c.readfrom_mem(device_addr, command, 2)
+    return data
+
+
+#def RESET_DEVICE(_i2c, device_addr):
--- GDK101_project/src/main.py
+++ GDK101_project/src/main.py
@@ -1,9 +1,16 @@
 from machine import I2C,Pin
 import ssd1306
 import fun
+import time
 
-GDK101_ADDR = 0X18
 LCD_ADDR = 0x3c
+GDK101_ADDR = 0X18
+RESET_SENZOR = 0XA0
+READ_STATUS = 0XB0
+READ_MEASURING_TIME = 0XB1
+READ_MEASURING_VALUE_TEN_MIN = 0XB2
+READ_MEASURING_VALUE_ONE_MIN = 0XB3
+READ_FIRMWARE_VERSION = 0XB4
 
 rst = Pin(16, Pin.OUT)
 rst.value(1)
@@ -15,8 +22,11 @@ i2c = I2C(scl=scl, sda=sda)
 
 lcd = ssd1306.SSD1306_I2C(128, 64, i2c)
 
-result = fun.DETECT_DEVICE(i2c, LCD_ADDR)
+result = fun.DATA_TRANSFER(i2c, GDK101_ADDR, READ_STATUS)
+# print("naslo sa " + str(result))
 print("naslo sa " + str(result))
+
+
 
 lcd.text("rslt "+ str(result), 0, 0)
 lcd.show()
